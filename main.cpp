943b14abf3f8359c82255ce4d911ef1b825edfd2
JuanFernandoUPS <jacostar2@est.ups.edu.ec>
2022-07-18 21:44:35 -0500

    Internalizacion del programa

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include "painter.h"
 
 #include <QApplication>
+#include <QLocale>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for(const QString &locale: uiLanguages){
+        const QString baseName = "Mi Paint_" + QLocale(locale).name();
+        if(translator.load(":/i18n/" + baseName)){
+            a.installTranslator(&translator);
+            break;
+        }
+    }
     Painter w;
     w.show();
     return a.exec();
